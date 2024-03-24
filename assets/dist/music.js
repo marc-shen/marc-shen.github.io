8fea57e0fd823d3bcdaff4499c26c434d208b199
Marc Shen <marcshen@163.com>
2024-03-24 22:06:46 +0800

    update

--- assets/dist/music.js
+++ assets/dist/music.js
@@ -10,7 +10,7 @@ const ap = new APlayer({
   mutex: true,
   listFolded: false,
   listMaxHeight: 90,
-  lrcType: 3,
+  // lrcType: 3,
   audio: [
     {
       name: "一生有你",
@@ -18,7 +18,7 @@ const ap = new APlayer({
       url: "https://rl-sycdn.kuwo.cn/8a2555a8c66ab51f2b389b8c01a69869/660029a8/resource/n3/92/78/870355913.mp3?from=vip",
       cover:
         "http://p2.music.126.net/4Tz-EfqUy0mbz6rJFRtrnA==/120946279073830.jpg?param=130y130",
-      lyric: "https://marc.stic.work/media/files/一生有你-水木年华.lrc",
+      // lyric: "https://marc.stic.work/media/files/一生有你-水木年华.lrc",
       theme: "#ebd0c2",
     },
     {
@@ -27,8 +27,8 @@ const ap = new APlayer({
       url: "https://lv-sycdn.kuwo.cn/20cea583ac8e86aaa6f8de318f5973ed/66002d6e/resource/30106/trackmedia/M500003hxirX20RmQa.mp3?from=vip",
       cover:
         "http://p2.music.126.net/lt4R_XbCZsT-yzRfWs9VfQ==/3434874331529456.jpg?param=130y130",
-      lyric:
-        "https://marc.stic.work/media/files/好好+(想把你写成一首歌)-五月天.lrc",
+      // lyric:
+      // "https://marc.stic.work/media/files/好好+(想把你写成一首歌)-五月天.lrc",
       theme: "#ebd0c2",
     },
     {
@@ -37,7 +37,7 @@ const ap = new APlayer({
       url: "http://music.163.com/song/media/outer/url?id=1338096419.mp3",
       cover:
         "http://p2.music.126.net/YbyP5f7ZSTlyDQiSkF5tbA==/109951163780883546.jpg?param=130y130",
-      lyric: "https://marc.stic.work/media/files/海棠花忆 - 封茗囧菌.lrcx",
+      // lyric: "https://marc.stic.work/media/files/海棠花忆 - 封茗囧菌.lrcx",
       theme: "#46718b",
     },
     {
@@ -46,7 +46,7 @@ const ap = new APlayer({
       url: "https://ra-sycdn.kuwo.cn/721fa5ccfc004f09f13588466d3862cf/66002df5/resource/n1/128/55/13/231276665.mp3?from=vip",
       cover:
         "http://p1.music.126.net/GoiTB6oG3vQWntnCjKRw0g==/109951163092691594.jpg?param=130y130",
-      lyric: "https://marc.stic.work/media/files/旅行-许巍.lrc",
+      // lyric: "https://marc.stic.work/media/files/旅行-许巍.lrc",
       theme: "#46718b",
     },
     {
@@ -55,7 +55,7 @@ const ap = new APlayer({
       url: "http://music.163.com/song/media/outer/url?id=1344088470.mp3",
       cover:
         "http://p2.music.126.net/WEvnP_ZexHMy4bHFLbhdAA==/109951163838777596.jpg?param=130y130",
-      lyric: "https://marc.stic.work/media/files/时光卷轴-封茗囧菌,双笙.lrc",
+      // lyric: "https://marc.stic.work/media/files/时光卷轴-封茗囧菌,双笙.lrc",
       theme: "#46718b",
     },
   ],
